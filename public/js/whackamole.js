325345c4f12aa1e3ab5aade6e804c367d951a777
Gaston Lenotre <gastonlenotre@gmail.com>
2016-02-24 20:43:03 -0600

    added rats and countdown timer

--- public/js/whackamole.js
+++ public/js/whackamole.js
@@ -1,29 +1,70 @@
 "use strict";
 
 $(document).ready(function(){
-	$(".rat").fadeOut();
+	$(".rat").fadeOut(1000);
 
-("p").click(function() {
-	index = 0;
-	randomRat();
-	showUp();
+	var rat = 0;
+	var timer = 30;
+	var timerInterval;
+	var gameInterval;
+	var gameStarted=false;
+	var index = 0;
+
+$("#play").click(function() {
+	
+	if(!gameStarted) {
+		var index = 0;
+		$("#count").html(index + ' whacks');
+		gameStarted = true;
+		randomRat();
+		showUp(rat);
+		gameInterval = setInterval(function() {
+			randomRat();
+			showUp(rat);
+		}, 4100)
+		timerInterval = setInterval(function() {
+			timer--;
+
+			// update displayed timer w/ jquery
+			if (timer >= 2) {
+				$("#timer").html(timer + ' seconds');	
+			}
+			if (timer == 1) {
+				$("#timer").html(timer + ' second');		
+			}
+
+			if (timer == 0) {
+				clearInterval(gameInterval);
+				clearInterval(timerInterval);
+				$("#timer").html(timer + ' seconds');		
+				gameStarted=false;
+				index = 0;
+			};
+
+		}, 1000);
+	} 
 });
 
 function randomRat(){
-	var rat = Math.floor((Math.random() * 9)+1);
-	numberRat.push(rat);
+	rat = Math.floor((Math.random() * 9)+1);
 }
 
-function showUp(){
-	$("#ratone").fadeIn();
-}
+function showUp(rat){
+	$('[data-id="' + rat + '"]').fadeIn(2000);
+	$('[data-id="' + rat + '"]').fadeOut(2000);
+};
 
-function dropOut(){
+$(".rat").click(function() {
+	index++;
+	// $(".rat").stop();
+	$(".rat").hide();
 
-}
-
-function scoreBoard(){
-
-}
+	if (index == 1) {
+		$("#count").html("1 whack");	
+	}
+	if (index >= 2) {
+		$("#count").html(index + ' whacks');	
+	}
+});
 
 });
