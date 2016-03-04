279bcb4ab53ef629a2d1d73feb65154e212a14ee
Gaston Lenotre <gastonlenotre@gmail.com>
2016-03-04 14:52:45 -0600

    added some headings

--- Readme.md
+++ Readme.md
@@ -1,1 +1,12 @@
- # Whackamole game here
+ # Whackarat Javascript Game
+
+ ## Technologies used
+
+ -HTML 5 for the structure of the game board
+ -CSS3 for styling
+ -Javascript for timers and logic
+ -jQuery for event listeners and DOM manipulation
+
+ ##Getting started
+
+ ## How to play
--- public/js/whackamole.js
+++ public/js/whackamole.js
@@ -1,70 +1,70 @@
 "use strict";
 
-$(document).ready(function(){
-	$(".rat").fadeOut(1000);
+$(document).ready(function() {
+    $(".rat").fadeOut(1000);
 
-	var rat = 0;
-	var timer = 30;
-	var timerInterval;
-	var gameInterval;
-	var gameStarted=false;
-	var index = 0;
+    var rat = 0;
+    var timer = 30;
+    var timerInterval;
+    var gameInterval;
+    var gameStarted=false;
+    var index = 0;
 
-$("#play").click(function() {
-	
-	if(!gameStarted) {
-		var index = 0;
-		$("#count").html(index + ' whacks');
-		gameStarted = true;
-		randomRat();
-		showUp(rat);
-		gameInterval = setInterval(function() {
-			randomRat();
-			showUp(rat);
-		}, 4100)
-		timerInterval = setInterval(function() {
-			timer--;
+$("#play").click(function() { 
+    if(!gameStarted) {                
+        $("#count").html(index + ' whacks');
+        timer = 30;
+        $("#timer").html(timer + ' seconds');
+        gameStarted = true;
+        randomRat();
+        showUp(rat);
+        gameInterval = setInterval(function() {
+            randomRat();
+            showUp(rat);
+        }, 4100);
+        timerInterval = setInterval(function() {
+            timer--;
 
-			// update displayed timer w/ jquery
-			if (timer >= 2) {
-				$("#timer").html(timer + ' seconds');	
-			}
-			if (timer == 1) {
-				$("#timer").html(timer + ' second');		
-			}
+            // update displayed timer w/ jquery
+            if (timer >= 2) {
+            $("#timer").html(timer + ' seconds');   
+            }
+            if (timer == 1) {
+            $("#timer").html(timer + ' second');        
+            }
 
-			if (timer == 0) {
-				clearInterval(gameInterval);
-				clearInterval(timerInterval);
-				$("#timer").html(timer + ' seconds');		
-				gameStarted=false;
-				index = 0;
-			};
+            if (timer == 0) {
+            clearInterval(gameInterval);
+            clearInterval(timerInterval);
+            $("#timer").html(timer + ' seconds');       
+            gameStarted=false;
+            index = 0;
+            }
 
-		}, 1000);
-	} 
+        }, 1000);
+    } 
 });
 
-function randomRat(){
-	rat = Math.floor((Math.random() * 9)+1);
+function randomRat() {
+    rat = Math.floor((Math.random() * 9)+1);
 }
 
-function showUp(rat){
-	$('[data-id="' + rat + '"]').fadeIn(2000);
-	$('[data-id="' + rat + '"]').fadeOut(2000);
-};
+function showUp(rat) {
+    $('[data-id="' + rat + '"]').fadeIn(2000);
+    $('[data-id="' + rat + '"]').fadeOut(2000);
+}
 
 $(".rat").click(function() {
-	index++;
-	// $(".rat").stop();
-	$(".rat").hide();
+    index++;
+    // $(".rat").stop();
+    $(".rat").hide();
 
-	if (index == 1) {
-		$("#count").html("1 whack");	
-	}
-	if (index >= 2) {
-		$("#count").html(index + ' whacks');	
-	}
+    if (index == 1) {
+        $("#count").html("1 whack");    
+    }
+    if (index >= 2) {
+        $("#count").html(index + ' whacks');    
+    }
 });
 
 });
